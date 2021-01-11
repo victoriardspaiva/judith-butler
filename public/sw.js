f79168a50daa91ae0d655c0ced3b31df7a53be1d
Victória Paiva <victoriardspaiva@recife.pe.senac.br>
2021-01-11 11:40:58 -0300

    make it better

--- public/sw.js
+++ public/sw.js
@@ -1,52 +1,42 @@
 var CACHE_NAME = 'staticV2';
 
-this.addEventListener('install', function(event) {
+this.addEventListener('install', function (event) {
     event.waitUntil(
-        caches.open(CACHE_NAME).then(function(cache) {
+        caches.open(CACHE_NAME).then(function (cache) {
             return cache.addAll([
                 './',
 
                 //
-               // '../css/materialize.css',
+                // '../css/materialize.css',
                 '../css/style.css',
-             //   '../css/materialdesignicons.css',
-             //   '../css/materialdesignicons.css.map',
-                 //
-                 '../js/jquery-3.2.1.min.js',
-              //   '../js/materialize.js',
-                 '../js/init.js',
-             //    '../js/camera.js',
-                 //
-                '../assets/img/img0.jpg',
-                '../assets/img/img1.jpg',
-                '../assets/img/img2.jpg',
-                '../assets/img/img3.jpg',
-                '../assets/img/img4.jpg',
-                '../assets/img/img5.jpg',
-                '../assets/img/img6.jpg',
-                '../assets/img/img7.jpg',
-                '../assets/img/img8.jpg',
-                '../assets/img/img9.jpg',
-                '../assets/img/img10.jpg',
-                '../assets/img/icon.ico',
-                '../assets/img/logo.png',
+                //  '../css/materialdesignicons.css',
+                //   '../css/materialdesignicons.css.map',
                 //
-                '../fonts/materialdesignicons-webfont.woff2?v=5.8.55',
+                '../js/jquery-3.2.1.min.js',
+                //   '../js/materialize.js',
+                '../js/init.js',
+                //    '../js/camera.js',
+                //
+                '../assets/img/profile.jpg',
+                '../assets/img/favicon.ico',
+
+                //
+                //   '../fonts/materialdesignicons-webfont.woff2?v=5.8.55',
                 //
                 '../manifest.json',
-             ]);
+            ]);
         })
     );
 });
 
 self.addEventListener('activate', function activator(event) {
     event.waitUntil(
-        caches.keys().then(function(keys) {
+        caches.keys().then(function (keys) {
             return Promise.all(keys
-                .filter(function(key) {
+                .filter(function (key) {
                     return key.indexOf(CACHE_NAME) !== 0;
                 })
-                .map(function(key) {
+                .map(function (key) {
                     return caches.delete(key);
                 })
             );
@@ -54,9 +44,9 @@ self.addEventListener('activate', function activator(event) {
     );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function (event) {
     event.respondWith(
-        caches.match(event.request).then(function(cachedResponse) {
+        caches.match(event.request).then(function (cachedResponse) {
             return cachedResponse || fetch(event.request);
         })
     );
